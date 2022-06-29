6130bfca0318730b649b962ba84e78e71a6879f8
Boubacar Sow <boubasow.pro@gmail.com>
2022-06-29 01:57:50 -0400

    Country controller

--- src/main/java/com/springboot/fleetapp/controllers/CountryController.java
+++ src/main/java/com/springboot/fleetapp/controllers/CountryController.java
@@ -1,7 +1,14 @@
 package com.springboot.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class CountryController {
+
+    @GetMapping("/country")
+    public String getCountries(){
+        return "country";
+    }
 }
